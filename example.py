de1b0bc6350881a197bce5eeb8387782f99e3dbb
Dmitry Batkovich <batya239@gmail.com>
2014-09-22 23:07:49 +0400

    example

---
+++ example.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf8
+
+__author__ = 'dima'
+
+import radosfs
+import modes
+from sys import argv
+
+username = argv[0]
+ceph_conf = argv[1]
+
+data_pool = argv[2]
+metadata_pool = argv[3]
+
+
+fs = radosfs.RadosFs(username, ceph_conf)
+fs.add_data_pool(data_pool, "/")
+fs.add_metadata_pool(metadata_pool, "/")
+fs.dir("/").create(-1, False, 1000, 1000)
+print fs.dir("/").is_writable()
+print fs.dir("/").is_readable()
+fs.file("/my.txt", modes.READ_WRITE).create(384, pool=data_pool)
+fs.file("/my1.txt", modes.READ_WRITE).create(384, pool=data_pool)
+fs.file("/my2.txt", modes.READ_WRITE).create(384, pool=data_pool)
+fs.dir("/").update()
+print fs.dir("/").entries()
+print fs.dir("/").entry(0)
+print fs.dir("/").entry(1)
+print fs.dir("/").entry(2)
+print fs.file("/my.txt", modes.READ_WRITE).mode() == modes.READ_WRITE
+fs.file("/my.txt", modes.READ_WRITE).remove()
+fs.file("/my1.txt", modes.READ_WRITE).remove()
+fs.file("/my2.txt", modes.READ_WRITE).remove()
