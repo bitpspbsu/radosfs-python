8a8dac90f7417d7acfc55571ec332544a22de4b2
Dmitry Batkovich <batya239@gmail.com>
2014-09-23 00:05:03 +0400

    example updated

--- example.py
+++ example.py
@@ -4,7 +4,6 @@
 __author__ = 'dima'
 
 import radosfs
-import modes
 from sys import argv
 
 username = argv[0]
@@ -20,15 +19,15 @@ fs.add_metadata_pool(metadata_pool, "/")
 fs.dir("/").create(-1, False, 1000, 1000)
 print fs.dir("/").is_writable()
 print fs.dir("/").is_readable()
-fs.file("/my.txt", modes.READ_WRITE).create(384, pool=data_pool)
-fs.file("/my1.txt", modes.READ_WRITE).create(384, pool=data_pool)
-fs.file("/my2.txt", modes.READ_WRITE).create(384, pool=data_pool)
+fs.file("/my.txt", radosfs.OpenMode.READ_WRITE).create(384, pool=data_pool)
+fs.file("/my1.txt", radosfs.OpenMode.READ_WRITE).create(384, pool=data_pool)
+fs.file("/my2.txt", radosfs.OpenMode.READ_WRITE).create(384, pool=data_pool)
 fs.dir("/").update()
 print fs.dir("/").entries()
 print fs.dir("/").entry(0)
 print fs.dir("/").entry(1)
 print fs.dir("/").entry(2)
-print fs.file("/my.txt", modes.READ_WRITE).mode() == modes.READ_WRITE
-fs.file("/my.txt", modes.READ_WRITE).remove()
-fs.file("/my1.txt", modes.READ_WRITE).remove()
-fs.file("/my2.txt", modes.READ_WRITE).remove()
+print fs.file("/my.txt", radosfs.OpenMode.READ_WRITE).mode() == radosfs.OpenMode.READ_WRITE
+fs.file("/my.txt", radosfs.OpenMode.READ_WRITE).remove()
+fs.file("/my1.txt", radosfs.OpenMode.READ_WRITE).remove()
+fs.file("/my2.txt", radosfs.OpenMode.READ_WRITE).remove()
