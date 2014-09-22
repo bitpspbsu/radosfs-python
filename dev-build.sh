e52fca580a8b9a53ed100382f9a524dc689ad1b0
Dmitry Batkovich <batya239@gmail.com>
2014-09-23 00:04:49 +0400

    local python build now works

--- dev-build.sh
+++ dev-build.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
 rm -rf radosfs/*.cpp
-rm -rf radosfs/*.so
 rm -rf build
-python setup.py build_ext --inplace
-cp radosfs/*.py ./
-rm -rf build
+python setup.py install --user
 rm -rf radosfs/*.cpp
