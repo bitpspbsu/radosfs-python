147fd6a86f87aa54766782a248f63c4bb91a7450
Dmitry Batkovich <batya239@gmail.com>
2014-09-23 08:52:31 +0400

    test stub

---
+++ radosfs_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf8
+
+__author__ = 'dima'
+
+import radosfs
+import unittest
+
+
+class RadosFsTest(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
