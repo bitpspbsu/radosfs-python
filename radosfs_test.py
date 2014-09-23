603e96fd55f947d6d3048141e0f3c911fba49791
Dmitry Batkovich <batya239@gmail.com>
2014-09-24 00:56:09 +0400

    added environment variables defs for tests

--- radosfs_test.py
+++ radosfs_test.py
@@ -5,6 +5,13 @@ __author__ = 'dima'
 
 import radosfs
 import unittest
+import os
+
+
+USER_NAME = os.environ["PRFS_USER_NAME"]
+CONFIG_PATH = os.environ["PRFS_CONFIG_PATH"]
+DATA_POOL = os.environ["PRFS_DATA_POOL_TEST"]
+META_DATA_POOL = os.environ["PRFS_META_DATA_POOL_TEST"]
 
 
 class RadosFsTest(unittest.TestCase):
