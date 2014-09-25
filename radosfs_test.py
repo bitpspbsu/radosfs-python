8149026db4e9117446a5b57091f18b7cae1d2ad0
Dmitry Batkovich <batya239@gmail.com>
2014-09-26 01:36:39 +0400

    added base tests

--- radosfs_test.py
+++ radosfs_test.py
@@ -15,7 +15,34 @@ META_DATA_POOL = os.environ["PRFS_META_DATA_POOL_TEST"]
 
 
 class RadosFsTest(unittest.TestCase):
-    pass
+    def test_creation_files_in_root_folder(self):
+        f1 = self.fs.file("/my1.txt", radosfs.OpenMode.READ_WRITE)
+        f1.create(384, pool=DATA_POOL)
+        f2 = self.fs.file("/my2.txt", radosfs.OpenMode.READ_WRITE)
+        f2.create(384, pool=DATA_POOL)
+        self.fs.dir("/").update()
+        self.assertEqual(["my1.txt", "my2.txt"], self.fs.dir("/").entries())
+        f1.remove()
+        f2.remove()
+
+    def test_read_write_to_file(self):
+        self.fs.dir("/").create(-1, False, 1000, 1000)
+        f1 = self.fs.file("/my1.txt", radosfs.OpenMode.READ_WRITE)
+        f1.create(384, pool=DATA_POOL)
+        f1.write("qwerty", 0)
+        self.assertEqual("qwerty", f1.read())
+        f1.remove()
+
+    def setUp(self):
+        self.fs = radosfs.RadosFs(USER_NAME, CONFIG_PATH)
+        self.fs.add_metadata_pool(META_DATA_POOL, "/")
+        self.fs.add_data_pool(DATA_POOL, "/", size=1 << 30)
+        self.fs.dir("/").create(-1, False, 1000, 1000)
+
+    def tearDown(self):
+        self.fs.dir("/").remove()
+        self.fs.remove_metadata_pool(META_DATA_POOL)
+        self.fs.remove_data_pool(DATA_POOL)
 
 
 if __name__ == "__main__":
